6f8f309c8c835219a66de45e7226a710de2641f2
Noercholis <poetnoer@gmail.com>
2014-08-30 01:28:04 +0700

    Create app.php

---
+++ app.php
@@ -0,0 +1,20 @@
+<?php
+  $app = new NUObject(array(
+    'init'=>true,
+    'db'=>new NUObject(array(
+      'driver'=>'mysql',
+      'host'=>'localhost',
+      'username'=>'root',
+      'password=>'root',
+      'database'=>'',
+      'connection'=>null,
+      'q'=>'',
+      'connect'=>function($self){
+        $self::$base->connection = mysqli($self::$base->host,$self::$base->username,$self::$base->password,$self::$base->database);
+      },
+      'setQuery'=>function($self,$q){
+        $self::$base->q=$q;
+      }
+    ))
+  ));
+?>
